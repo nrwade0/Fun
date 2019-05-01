da15a2cbf890d1a8638b6054714ce02ba5430612
Nick Wade <nrwade0@frostburg.edu>
2019-05-01 08:45:28 -0400

    Optimize the runtime for PE5
    Fixes the range of text values, iteration count, and the ending condition

--- Project Euler/pe5.m
+++ Project Euler/pe5.m
@@ -1,42 +1,42 @@
-
-% Project Euler prob 5
-
+% Project Euler problem 5
 
 %{
-
-2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
-
-What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
-
+2520 is the smallest number that can be divided by each of the numbers
+ from 1 to 10 without any remainder.
+What is the smallest positive number that is evenly divisible by all of 
+ the numbers from 1 to 20?
 %}
+
 tic;
 
-N = 1e4;
+% Starts at 2520 and increment by 2520
+N = 3e9;
 
-% divisors
-range = [3 4 5 6 7 8 9];%11 13 15 17 19];
+% DIVISORS
+% Must keep 20 but not 2 or 10 or 5.
+% 19, 17, 13, 11 are primes.
+% keep 18 and leave out 12 and 6 and 3 and 2.
+% 14 can leave out 7 and 2.
+range = [11 13 14 16 17 18 19 20];
+val = 20;
 
-
-for i = 10:2:N
-    
+while (val < 1e10)
     count = 0;
-    if(mod(i,2) == 0) % evens only
-        
-        for j = 1:4
-            if(mod(i,range(j)) == 0)
-                count = count + 1;
-                if(count == 4)
-                    ans = i
-                    break;
-                end
-                
-            else
-                break;
-            end
-            
+    
+    for i = 1:length(range)
+        if(mod(val, range(i)) == 0)
+            count = count + 1;
+        else
+            break;
         end
-        
     end
+    
+    if(count == length(range))
+        disp(val)
+        break;
+    end
+    
+    val = val + 20;
     
 end
 
