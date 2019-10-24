d04765e4cee0614d956b4a81d942c03d9ee512f7
Nick <nrwade0@gmail.com>
2019-10-24 10:59:26 -0400

    Add all challenge files

---
+++ UMDChallengeBox/fancy-fence/compileFortran.command
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd ~/Documents/GitHub/fun/UMDChallengeBox/fancy-fence
+gfortran -o fancy_fence fancy_fence.f90
--- UMDChallengeBox/fancy-fence/fancy_fence.f90
+++ UMDChallengeBox/fancy-fence/fancy_fence.f90
@@ -1,84 +1,126 @@
 ! paste into command line: "gfortran -o fancy_fence fancy_fence.f90"
 ! link: https://challengebox.cs.umd.edu/2019/Fence/index.html
 !
+! fancy_fence program calculates the convex hull of a list of points
+!  (xy coordinates) inputted in data files
 !
 !
-program PE1
-!  implicit none
-  integer :: n, p0_x, p0_y, temp_x, temp_y, p0_index
-  logical :: exist_file
-  character(*), parameter :: file_path = '/Users/nick/Documents/GitHub/fun/UMDChallengeBox/fancy-fence/'
-  real, dimension(3,3) :: coords, array ! (cols, rows)
-  real, dimension(1,3) :: polar
+program fancy_fence
+!   implicit none
+   integer :: n, H
+   logical :: exist_file
+   integer, dimension(2,11) :: S ! (cols, rows)
+   integer, dimension(2,1) :: p
+   integer :: a(3,3) = reshape((/1,2,3,4,5,6,7,8,9/), (/3,3/))
 
-!  write(*,*) ''
-!  write(*,*) 'Fancy Fence'
-!  write(*,*) 'UMD Challenge Box, Oct. 14-16 2019'
+!  Print header
+   write(*,*) ''
+   write(*,*) 'Fancy Fence'
+   write(*,*) 'UMD Challenge Box, Oct. 14-16 2019'
 
-! Open the input file
-  open(10, file=file_path//'fence-test0.txt', status='OLD')
-  write(*,*) "opened file"
+!  Open the file and save to 'n' and 'S'
+   call open_file(n, S)
 
-! Read the coordinates and place in 'n' and 2D array 'coords'
-  read(10, *) n
-  read(10, *) coords
+!  Print data to check if correct
+   write(*,*) 'n =', n
+   write(*,*) 'S ='
+   do 2 i = 1, n
+      write(*,*) "(", S(1,i), ", ", S(2,i), ")"
+2  end do
 
-! Close the input file, not needed anymore
-  close(10)
+!  Determine leftmost point
+   call left_index(p, S, n)
+   write(*,*) 'p =', p
 
-! Print data to check if correct
-  write(*,*) 'n = ', n
-  write(*,*) 'coords = '
-  do 2 i = 1, n
-    write(*,*) "(", coords(1,i), ", ", coords(2,i), ")"
-2 end do
+!  Determine orientation of three points
 
-! Graham Scan for Convex Hull
-! https://en.wikipedia.org/wiki/Graham_scan
-
-! Find 'p0' w/ lowest x-coord and y-coord
-  ! Set temporary values for finding p0 to first xy pair in coords
-  temp_x = coords(1, 1)
-  temp_y = coords(1, 2)
-  do 3 i = 2, n
-    ! if there is a new smaller y-coordinate, replace our p0 values
-    if(coords(i, 2) .lt. temp_y) then
-      temp_x = coords(i, 1)
-      temp_y = coords(i, 2)
-      p0_index = i
-    end if
-    ! if the y-coords are the same, then choose the lower x-coordinate
-    if(coords(i,1) .lt. temp_x .and. coords(i, 2) .eq. temp_y) then
-      temp_x = coords(i, 1)
-      temp_y = coords(i, 2)
-    end if
-3 end do
-  ! set our temp values to official p0 coordinates and print to screen
-  p0_x = coords(1, p0_index)
-  p0_y = coords(2, p0_index)
-  write(*,*) 'p0_x = ', p0_x, 'p0_y = ', p0_y
-
-
-! sort array 'coords' based on the polar angle with p0, atan2(y,x) should
-!  work. Keep it on the 'stack'. If several points have the same polar angle
-!  then keep the farthest.
-  ! Determine polar angles for each coordinate relative to p0, saved in 'polar'
-  do 4 i = 1, n
-    coords(3,i) = atan2(real(coords(1,i)), real(coords(2,i)))*180/3.14159
-4 end do
-  write(*,*) coords
-  ! sort the polar array
-
-  ! cycle thru the points and snatch ones that follow these rules:
-  !  1. There is at least one point on the stack still.
-  !  2. and ccw(next_to_top(stack), top(stack), point) < 0
-  !  # pop the last point from the stack if we turn clockwise to reach this point
+   !H = 0
+   !call orientation(a, b, c, H)
 
 
 
+end program fancy_fence
 
 
 
+! ---------------------------------------------------
+! subroutine open_file inputs data into n and S
+! ---------------------------------------------------
+subroutine open_file(n, S)
+!   implicit none
+   character(*), parameter :: file_path = '/Users/nick/Documents/GitHub/fun/UMDChallengeBox/fancy-fence/data/'
+   integer :: n
+   integer, dimension(2,11) :: S
+
+!  Open the input file
+   open(10, file=file_path//'fence-test1.in.txt', status='OLD', iostat=ios)
+
+!  Notify user on the condition of data file input
+   if(ios .ne. 0) then
+     write(*,*) 'An error while opening the data file.'
+   else
+     write(*,*) "The file was opened correctly."
+   endif
+
+!  Read the number of points and coordinates and place in 'n' and 2D array 'S'
+   read(10, *) n
+   do 11 i = 1, n
+     read(10, 15) S(1,i), S(2,i)
+11 end do
+15 format(I2,I2)
+
+!  Close the input file, not needed anymore
+   close(10)
+
+end subroutine open_file
 
 
-end program PE1
+
+! -----------------------------------------------------------------
+! subroutine left_index determines the leftmost point p in a set S
+! -----------------------------------------------------------------
+subroutine left_index(p, S, n)
+!   implicit none
+   integer :: n
+   integer, dimension(2,11) :: S
+   integer, dimension(2,1) :: p
+
+   ! initialize p
+   p(1,1) = S(1,1)
+   p(2,1) = S(2,1)
+   do 20 i = 1, n
+     if(p(1,1) .gt. S(1,i)) then
+       p(1,1) =  S(1,i) ! p_x
+       p(2,1) =  S(2,i) ! p_y
+     endif
+20 enddo
+
+end subroutine left_index
+
+
+
+! -----------------------------------------------------------------
+! subroutine orientation computes orientation of three points
+! -----------------------------------------------------------------
+subroutine orientation(a, b, c, H)
+!   implicit none
+   integer :: H
+   real :: val
+   integer, dimension(2,1) :: a, b, c
+
+   ! val = (y2 - y1)*(x3 - x2) - (y3 - y2)*(x2 - x1)
+   val = (b(2,1) - a(2,1))*(c(1,1) - b(1,1)) - (c(2,1) - b(2,1))*(b(1,1) - a(1,1))
+
+   ! determine the orientation of the three points
+   if(val .gt. 0) then
+     H = 1  ! clockwise
+   elseif (val .eq. 0) then
+     H = 0  ! colinear
+   elseif (val .lt. 0) then
+     H = -1 ! counterclockwise
+   end if
+
+   write(*,*) 'H = ', H
+
+
+end subroutine orientation
