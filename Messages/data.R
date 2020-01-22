d4b1cd4ce895f0d3932e17e2b5dc638845dcf9c8
Nick <nrwade0@gmail.com>
2020-01-21 22:04:25 -0500

    Update python and R code
    python more likely route....

--- Messages/data.R
+++ Messages/data.R
@@ -1,16 +1,5 @@
 library(RSQLite)
 library(tidyverse)
+library(ProjectTemplate)
 
-install.packages("dbplyr")
-library(dplyr)
-
-conn <- DBI::dbConnect(RSQLite::SQLite(), path = "chat.db")
-
-# Write the mtcars dataset into a table names mtcars_data
-dbWriteTable(conn, "dat", mtcars)
-
-# List all the tables available in the database
-dbListTables(conn)
-
-
-dbGetQuery(conn, "SELECT * FROM dat")
+db.reader('chat.db', 'chat.db', 'dat')
--- Messages/gather_data.py
+++ Messages/gather_data.py
@@ -3,21 +3,23 @@
 """
 Created on Mon Jan 20 19:21:01 2020
 
+https://towardsdatascience.com/heres-how-you-can-access-your-entire-imessage-history-on-your-mac-f8878276c6e9
+
 @author: nick
 """
 
-# connection code
+
+# import libraries
 import sqlite3
 import pandas as pd
 
-# substitute username with your username
-conn = sqlite3.connect('chat.db')
 
+# establish connection to .db file and point cursor
+conn = sqlite3.connect('chat.db')
 cursor = conn.cursor()
 
-cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-
-#print(cursor.fetchall())
+# get the names of the tables in the database
+cursor.execute("SELECT name FROM sqlite_master WHERE type = 'table';")
 
 # get messages and handles
 messages = pd.read_sql_query("select * from message limit 10", conn)
@@ -28,12 +30,3 @@ messages.rename(columns={'ROWID' : 'message_id'}, inplace = True)
 handles.rename(columns={'id' : 'phone_number', 'ROWID': 'handle_id'},
                inplace = True)
 
-merge_level_1 = temp = pd.merge(messages[['text', 'handle_id', 'date','is_sent',
-                                         'message_id']],  handles[['handle_id',
-                                                      'phone_number']], on ='handle_id', how='left')
-
-# get the chat to message mapping
-chat_message_joins = pd.read_sql_query("select * from chat_message_join", conn)
-
-# and join back to the merge_level_1 table
-df_messages = pd.merge(merge_level_1, chat_message_joins[['chat_id', 'message_id']], on = 'message_id', how='left')
